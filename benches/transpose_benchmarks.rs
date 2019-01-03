bf9e35efce103779791119b20c2f65c9dc082540
Elliott Mahler <join.together@gmail.com>
2019-01-02 22:27:47 -0800

    Tweaked benchmarks, re-added inplace benchmarks

--- benches/transpose_benchmarks.rs
+++ benches/transpose_benchmarks.rs
@@ -5,12 +5,12 @@ extern crate transpose;
 
 use criterion::{Criterion, ParameterizedBenchmark, Throughput};
 use std::mem;
+use std::time::Duration;
 
-fn bench_transpose<T: Copy + Default>(c: &mut Criterion, tyname: &str) {
-    let ref sizes = [(4, 4), (8, 8), (16, 16), (64, 64), (256, 256), (1024, 1024), (4096, 4096)];
+fn bench_oop_transpose<T: Copy + Default>(c: &mut Criterion, tyname: &str) {
+    let ref sizes = [(4, 4), (8, 8), (16, 16), (64, 64), (256, 256), (1024, 1024)];
 
-    let bench = ParameterizedBenchmark::new(
-        format!("transpose {}", tyname),
+    let bench = ParameterizedBenchmark::new(tyname,
         |b, &&(width, height)| {
             let mut buffer = vec![T::default(); width * height];
             let mut scratch = vec![T::default(); width * height];
@@ -18,15 +18,41 @@ fn bench_transpose<T: Copy + Default>(c: &mut Criterion, tyname: &str) {
             b.iter(|| { transpose::transpose(&mut buffer, &mut scratch, width, height); });
         },
         sizes)
-        .throughput(|&&(width, height)| Throughput::Bytes((width * height * mem::size_of::<T>()) as u32));
+        .throughput(|&&(width, height)| Throughput::Bytes((width * height * mem::size_of::<T>()) as u32))
+        .warm_up_time(Duration::from_secs(1));
 
-    c.bench("square transposes", bench);
+    c.bench("square transposes out-of-place", bench);
 }
 
-fn bench_u8(c: &mut Criterion) { bench_transpose::<u8>(c, "u8") }
-fn bench_u32(c: &mut Criterion) { bench_transpose::<u32>(c, "u32") }
-fn bench_u64(c: &mut Criterion) { bench_transpose::<u64>(c, "u64") }
-fn bench_u128(c: &mut Criterion) { bench_transpose::<u128>(c, "u128") }
+fn bench_oop_u32(c: &mut Criterion) { bench_oop_transpose::<u32>(c, "u32") }
+fn bench_oop_u64(c: &mut Criterion) { bench_oop_transpose::<u64>(c, "u64") }
 
-criterion_group!(benches, bench_u8, bench_u32, bench_u64, bench_u128);
-criterion_main!(benches);
+criterion_group!(out_of_place_benches, bench_oop_u32, bench_oop_u64);
+
+
+
+
+
+fn bench_inplace_transpose<T: Copy + Default>(c: &mut Criterion, tyname: &str) {
+    let ref sizes = [(4, 4), (8, 8), (16, 16), (64, 64), (256, 256), (1024, 1024)];
+
+    let bench = ParameterizedBenchmark::new(tyname,
+        |b, &&(width, height)| {
+            let mut buffer = vec![T::default(); width * height];
+            let mut scratch = vec![T::default(); std::cmp::max(width, height)];
+
+            b.iter(|| { transpose::transpose_inplace(&mut buffer, &mut scratch, width, height); });
+        },
+        sizes)
+        .throughput(|&&(width, height)| Throughput::Bytes((width * height * mem::size_of::<T>()) as u32))
+        .warm_up_time(Duration::from_secs(1));
+
+    c.bench("square transposes inplace", bench);
+}
+
+fn bench_inplace_u32(c: &mut Criterion) { bench_inplace_transpose::<u32>(c, "u32") }
+fn bench_inplace_u64(c: &mut Criterion) { bench_inplace_transpose::<u64>(c, "u64") }
+
+criterion_group!(inplace_benches, bench_inplace_u32, bench_inplace_u64);
+
+criterion_main!(out_of_place_benches, inplace_benches);
